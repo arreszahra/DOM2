af0a59f71dbf52884e503da08ce49725393ecb0d
Zahra Arres <arreszahra0@gmail.com>
2024-04-28 21:03:40 +0100

    second commit

--- dom.js
+++ dom.js
@@ -1,14 +1,16 @@
+DOMContentLoaded
 
-
-var container=document.getElementsByClassName(".container")
+var container=document.getElementsByClassName (".container")
 var color_box =document.getElementById("#color-box")
 var change_color_btn = document.getElementById("#change-color-btn")
 
 
-function getRandomColor(color_box,change_color_btn ){
-    change_color_btn.addeventlistener("click",function(){
-        color_box.style.color='orange'
-    })
+color_box.onclick=getRandomColor;
 
+
+function getRandomColor(color_box,change_color_btn){
+    
+    document.color_box.style.backgroundColor = "red";
 }
+color_box.addEventListener("click",getRandomColor);
      
